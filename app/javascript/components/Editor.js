e3c73469cd21297169101c086309761946191dd4
Ritesh Kumar <ritesh@emerald.pink>
2020-01-23 21:55:34 +0800

    now just need to figure out how to extract out the proper tasks array, almost there :)

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -26,7 +26,9 @@ class Editor extends React.Component {
     this.deleteTask = this.deleteTask.bind(this);
     this.updateTask = this.updateTask.bind(this);
 
-    console.log("[Editor.js]: |constructor method| editor component has been contructed");
+    console.log(
+      "[Editor.js]: |constructor method| editor component has been contructed"
+    );
   }
 
   // =========================  API FETCHING UPON MOUNT ================================
@@ -40,8 +42,14 @@ class Editor extends React.Component {
       .get("/api/tasks.json")
       .then(response => {
         this.setState({ tasks: response.data });
-        console.log("[Editor.js] |componentDidMount Hook| axios response for get /api/tasks.json: ", response.data);
-        console.log("[Editor.js] |componentDidMount Hook| current state in Editor.js: ", this.state);
+        console.log(
+          "[Editor.js] |componentDidMount Hook| axios response for get /api/tasks.json: ",
+          response.data
+        );
+        console.log(
+          "[Editor.js] |componentDidMount Hook| current state in Editor.js: ",
+          this.state
+        );
       })
       .catch(error => {
         console.log(error);
@@ -138,29 +146,41 @@ class Editor extends React.Component {
 
   render() {
     /*
-            What is encapsulated: 
-            - tasks: API output for /tasks.json
-            - tags: API output for /tags.json
-            - match : holds the input params (browser's requested url params)
-            */
+                What is encapsulated: 
+                - tasks: API output for /tasks.json
+                - tags: API output for /tags.json
+                - match : holds the input params (browser's requested url params)
+                */
     const { tasks } = this.state;
     const { tags } = this.state;
-    console.log("[Editor.js] |render method| from within editor.js, const {tasks} holds:", tasks);
-    console.log("[Editor.js] |render method| from within editor.js, const {tags} holds:", tags);
+    console.log(
+      "[Editor.js] |render method| from within editor.js, const {tasks} holds:",
+      tasks
+    );
+    console.log(
+      "[Editor.js] |render method| from within editor.js, const {tags} holds:",
+      tags
+    );
     //   null check just in case
     if (tasks === null) return null;
     if (tags === null) return null;
     const { match } = this.props;
-    console.log("[Editor.js] |render method| URL props held in const {match} = this.props", match);
+    console.log(
+      "[Editor.js] |render method| URL props held in const {match} = this.props",
+      match
+    );
 
     // url info:
     // need not necessarily be taskID, if tags, then it's tagID
     console.log("[Editor.js] : URL params in match.params.id", match.params);
     const taskId = match.params.id;
     const task = tasks.find(e => e.id === Number(taskId));
-    const tag = tags.find(e => e.id === Number(taskId)); // #debug 
+    const tag = tags.find(e => e.id === Number(taskId)); // #debug
 
-    console.log("[Editor.js] |render method| this.state.tasks from editor.js:", this.state.tasks);
+    console.log(
+      "[Editor.js] |render method| this.state.tasks from editor.js:",
+      this.state.tasks
+    );
     console.log("**** tasks:", tasks);
     console.log("**** task:", task);
     console.log("**** tags:", tags);
@@ -172,14 +192,15 @@ class Editor extends React.Component {
       <div>
         <Header />{" "}
         {/* Keep routes in this order:
-                                                                1. new
-                                                                2. edit
-                                                                3.delete
-                                                                4.display
-                                                                */}{" "}
+                                                                                1. new
+                                                                                2. edit
+                                                                                3.delete
+                                                                                4.display
+                                                                                */}{" "}
         <div className="grid">
           <div>
-            {/* this calls the TaskList and TagList components */}
+            {" "}
+            {/* this calls the TaskList and TagList components */}{" "}
             <TaskList tasks={tasks} activeId={Number(taskId)} />{" "}
             <TagList tags={this.state.tags} />{" "}
           </div>{" "}
@@ -201,10 +222,10 @@ class Editor extends React.Component {
               {" "}
               {/*  urm this needs tobe removed idk why it's here... */}{" "}
               {/* <PropsRoute
-                                                path="/tasks/new"
-                                                component={TaskForm}
-                                                onSubmit={this.addTask}
-                                              />{" "} */}{" "}
+                                                                path="/tasks/new"
+                                                                component={TaskForm}
+                                                                onSubmit={this.addTask}
+                                                              />{" "} */}{" "}
               <PropsRoute
                 exact
                 path="/tasks/:id/edit"
--- app/javascript/components/Tag.js
+++ app/javascript/components/Tag.js
@@ -1,29 +1,74 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 import { Link } from "react-router-dom"; // to pass existing props to tag form when editing
 import { tag_list } from "./../helpers/helpers";
 import { task_list } from "./../helpers/helpers";
 
-const Tag = ({ tag }) => (
-  // <div className="taskContainer">
-  //   {   console.log("*** props received by Tag.js:", tag)}
-  //   {" "}
-  //   {console.log("props received by Tag.js: ", tag)} <h2> {tag.name}</h2>{" "}
-  //   <ul>
-  //     <li>
-  //       <strong> Associated Tasks: </strong>{" "}
-  //     </li>{" "}
-  //     {/* <li className="task_list"> {task_list(tag)} </li>{" "} */}
-  //   </ul>{" "}
-  // </div>
-  <div>
-  {console.log("[Tag.js]: Props Received:", tag)}
-    YAY WE REACHED THE TAG COMPONENT.
-  </div>
-);
+class Tag extends React.Component {
+  constructor(props) {
+    // init the state:
+    super(props);
+    this.state = {
+      tags: null
+    };
+
+    // bind class methods here:
+
+    console.log(
+      "[Tag.js]: |constructor method| Tag component has been constructred"
+    );
+  }
+
+  // didMount hook: API fetching:
+  componentDidMount() {
+    console.log(
+      "[Tag.js] |componentDidMount Hook|componentDidMount hook is now running after the Tag component has been mounted..."
+    );
+    axios
+      .get("/api/tags.json")
+      .then(response => {
+        this.setState({ tags: response.data });
+        // console.log("axios response for get /api/tags.json", response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    console.log(
+      "[Tag.js]: |Axios API fetching| API for all tags has been pulled by Tag.js"
+    );
+    console.log(
+      "[Tag.js]: |Axios API fetching| current state:",
+      this.state.tags
+    );
+  }
+
+  // render method:
+  render() {
+    // encapsulate:
+    //  - tags
+    //  - match
+    const { tags } = this.state;
+    const { match } = this.props;
+    console.log("[Tag.js] |render method| URL related: {match}:", match);
+    console.log(
+      "[Tag.js] |render method| from within Tag.js, const {tags} holds:",
+      tags
+    );
+    console.log("[Tag.js] : URL params in match.params.id", match.params);
+    const tagId = match.params.id;
+    console.log("[Tag.js]: this is tagId: ", tagId);
+    console.log(
+      "[Tag.js]: this is what is in this.state.tags:",
+      this.state.tags
+    );
+    return <div>testing tag.js now just need to figure out how to extract out the correct tasks...</div>;
+  }
+}
 
 Tag.propTypes = {
   tag: PropTypes.shape()
+  // history: PropTypes.shape({ push: PropTypes.func }).isRequired,
 };
 
 Tag.defaultProps = {
